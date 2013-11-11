1fc372d318c0ccd4ecf94b03b4fc725d0f344de5
mikkel <mikkel@adapt.dk>
2013-11-12 00:31:20 +0100

    Handle db dumps

---
+++ sql/updatedb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/pre_kf_download.sql
+
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/kf_download.dmp

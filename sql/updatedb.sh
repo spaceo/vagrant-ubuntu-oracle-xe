9bc500d6e0e6e810c2f4844c868ec982cb99f823
mikkel <mikkel@adapt.dk>
2013-11-12 10:44:11 +0100

    Last changes

--- sql/updatedb.sh
+++ sql/updatedb.sh
@@ -3,3 +3,8 @@
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/pre_kf_download.sql
 
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/kf_download.dmp
+
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/pre_weblog.sql
+
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/weblog.dmp
+

823292ca5e6940f1726220c0392c47bfa4be3b4c
mikkel <mikkel@adapt.dk>
2013-11-12 10:58:36 +0100

    Fixed weblog sqlplus connection params in updatedb.sh

--- sql/updatedb.sh
+++ sql/updatedb.sh
@@ -4,7 +4,7 @@ echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf
 
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/kf_download.dmp
 
-echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/pre_weblog.sql
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus weblog_test/weblog_test@//localhost:1521/XE  @/vagrant/sql/pre_weblog.sql
 
-echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/weblog.dmp
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus weblog_test/weblog_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/weblog.dmp
 

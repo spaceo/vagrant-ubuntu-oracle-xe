6040d0232cb7e562e08856f16f6b700de1d2cee4
mikkel <mikkel@adapt.dk>
2013-11-13 10:06:01 +0100

    Adding comments to updatedb.sh in order to understand wtf is going on

--- sql/updatedb.sh
+++ sql/updatedb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Users; Prepare import (truncate etc.)
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/pre_kf_download.sql
-
+# Users; Import user tables.
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/kf_download.dmp
-
+# Permissions (truncate etc.)
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus weblog_test/weblog_test@//localhost:1521/XE  @/vagrant/sql/pre_weblog.sql
-
+# Permissions; IMport permissions.
 echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus weblog_test/weblog_test@//localhost:1521/XE  @/vagrant/sql/gst_dumps/weblog.dmp
+# Users; Sync smuser and smuser_extra from postgres.
+echo "EXIT;" | NLS_LANG=AMERICAN_AMERICA.UTF8 rlwrap sqlplus kf_download_test/kf_download_test@//localhost:1521/XE  @/vagrant/sql/smuser.sql
 
